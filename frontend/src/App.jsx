6f20f334c1ca974cce856396a8615c4a921e04b9
FARSEENA <farseenakazeez187@gmail.com>
2026-05-14 07:54:30 +0530

    Initial project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+ 
+
+  return (
+    <div className="bg-blck text-black h-screen flex items-center justify-center">
+    <h1 className="text-4xl font-bold">Online Book Store</h1>
+      
+    </div>
+  )
+}
+
+export default App
